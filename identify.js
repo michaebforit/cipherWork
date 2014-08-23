d7c444197a3543059ac44a007a6c7dd3c3dfeb4e
Michael <mike-bailey@users.noreply.github.com>
2014-08-23 12:57:15 -0400

    Update identify.js

--- identify.js
+++ identify.js
@@ -37,7 +37,54 @@ function idcipher(decodethis) {
 		// Ben Alpert wrote this next line, and I modified it a bit
 		var rot13fixed = decodethis.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}); 
 		document.getElementById("log").innerHTML += "<br/> Blind ROT13 decrypt resolves: " + rot13fixed;
+		var potentialbinary = decodethis;
+		var smellslikebinary = potentialbinary.replace(/ /g,"");
+		
+		
+		
+		
 		ishash(decodethis);
+		document.getElementById("log").innerHTML += "<br/>Processing " + smellslikebinary;
+		if ((/^[0,1]+$/.test(smellslikebinary))==true) {
+		document.getElementById("log").innerHTML += "<br/>SMELLS LIKE BINARRRYYYYY";
+		document.getElementById("log").innerHTML += "<br/>Processing without any spaces";
+		isitbinary(smellslikebinary);
+		} else {
+		document.getElementById("log").innerHTML += "<br/>Smells like no binary. :(";
 		}
 		
 		
+
+		}
+		
+		/*
+		Drop extra code here:
+		potentialbinary.replace(" ","")
+		/^[0,1]+$/.test()
+		*/
+		function isitbinary(bin) {
+		if ((bin.length / 8)==Math.round(bin.length / 8)) {
+		document.getElementById("log").innerHTML += "</br>" + bin.length;
+		document.getElementById("log").innerHTML += (bin.length / 8);
+		document.getElementById("log").innerHTML += Math.round(bin.length / 8);
+		document.getElementById("log").innerHTML += "<br/><b>Valid length for binary message</b>";
+		decodeBinary(bin,true);
+		}
+		
+		}
+		function decodeBinary(data,clean) {
+			if (clean==true) {
+					for (i = 0; i <= data.length;) {
+								
+								var convertbyte = data.substring(i,i+7);
+								var binary = ["1100001","1000001","1100010","1000010","1100011","1000011","1100100","1000100","1100101","1000101","1100110","1000110","1100111","1000111","1101000","1001000","1101001","1001001","1101010","1001010","1101011","1001011","1101100","1001100","1101101","1001101","1101110","1001110","1101111","1001111","1110000","1010000","1110001","1010001","1110010","1010010","1110011","1010011","1110100","1010100","1110101","1010101","1110110","1010110","1110111","1010111","1111000","1011000","1111001","1011001","1111010","1011010","01111010","01011010"];
+								var charset = ["a","A","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z"];
+								alert(convertbyte);
+								alert(binary.indexOf(convertbyte));
+								
+								var binconvert = binary.indexOf(convertbyte);
+								document.getElementById("log").innerHTML += charset[binconvert];
+								
+						}
+			}
+		}
